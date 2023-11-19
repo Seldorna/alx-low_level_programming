23e4b7e0c423dc7ef1595244c9ce7e795f468049
Seldorna <seldornakullie@gmail.com>
2023-11-19 08:15:32 -0800

    Edited a main.h file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@
 /*Function prototypes*/
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
