0c81e1e8a6017c87790e8c0c72c6a7041b7a2bc8
Seldorna <seldornakullie@gmail.com>
2023-11-22 13:29:38 -0800

    Edited

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,26 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
- * print_name - Prints each character in a string using a specified function.
- * @name: The input string.
- * @f: A function pointer to the function used to print each character.
- */
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-while (*name != '\0')
-{
-f(*name);
-name++;
-}
-}
+if (name == NULL || f == NULL)
+return;
 
-/**
- * print_char - Prints a character to the standard output.
- * @c: The character to be printed.
- */
-void print_char(char c)
-{
-putchar(c);
+f(name);
 }
-
